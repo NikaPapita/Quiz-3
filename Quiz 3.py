19500b0a5b02ab780ee7098a9e5eeae6104ea8c0
User <nika.papitashvili.1@btu.edu.ge>
2021-05-15 17:44:08 +0400

    First Commit

---
+++ Quiz 3.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+# 1
+
+# res = requests.get("https://dog.ceo/api/breeds/list/all")
+# print(res.status_code)
+# print(res.headers)
+# print(res.text)
+
+# 2
+
+# res = requests.get("https://dog.ceo/api/breeds/list/all")
+# json_res = res.text
+# load_json = json.loads(json_res)
+# structure = json.dumps(load_json, indent = 3)
+
+
+# 3
+
+# res = requests.get("https://dog.ceo/api/breeds/list/all")
+# json_res = res.text
+# load_json = json.loads(json_res)
+# struc = json.dumps(load_json, indent = 3)
+# print(struc)
